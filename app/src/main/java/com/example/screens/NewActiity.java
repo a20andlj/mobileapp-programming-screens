187fadc3b2d77071bdd930e150d5dd2393318cce
Andreas <a20andlj@student.his.se>
2021-04-26 12:02:46 +0200

    skapat ny aktivitet

---
+++ app/src/main/java/com/example/screens/NewActiity.java
@@ -0,0 +1,14 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewActiity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_actiity);
+    }
+}
